d78f72b858fa2ac71e402ceb3113e0674a5e34d4
km7dev <gavin@t8er.com>
2025-03-08 15:34:45 -0500

    aaaa

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/CCActionEase.hpp>
+
+class $modify (CCActionEase) {
+  void update(float dt) {
+    log::info("Test - {}", typeid(this).name())
+  }
+}
